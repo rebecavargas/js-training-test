01902e2dd78b1f2e579558f2d30b9a67053326a0
rebecavargas <Rebeca.Vargas@fundacion-jala.org>
2015-07-24 18:28:59 -0400

    Update SampleForm for problem with github

--- jquery-exercise/SampleForm.js
+++ jquery-exercise/SampleForm.js
@@ -1,19 +1,63 @@
 
 /*que tiene un método para manejar cada campo de la forma dada.*/
-
 var SampleForm = function(){
-	
-}
-
-myForm = new SampleForm();
-myForm.setHobbies('Skiing', 'Running')
 
 
-myForm.setName($('[name="fullname"]').val("Rebeca Vargas"))
 
-myForm.setEmail($('[name="email"]').val("rebecavg@gmail.com"))
+	this.hobbies = {
+		'hobby1': 'ski',
+		'hobby2': 'run',
+		'hobby3': 'eat',
+		'hobby4': 'sleep',
+		'hobby5': 'read',
+		'hobby6': 'code'
 
-myForm.selectGender($('[name="gender"][value="F"]').click())
 
-myForm.selecthobby1($('[name="hobbies[]"][id="hobby1"]').click())
-myForm.selecthobby2($('[name="hobbies[]"][id="hobby2"]').click())
+	};
+};
+
+
+	SampleForm.prototype.SetName = function(value){
+		var name = $('[name="fullname"][data-parsley-id="4"]');
+		name.val(value);
+	};
+	SampleForm.prototype.SetEmail = function(value)
+	{
+		var email = $('[name="email"]');
+		email.val(value);
+	}
+	SampleForm.prototype.SelectGender = function(value)
+	{
+		var gender = $('[name="gender"][value="F"]').click();
+	}
+	SampleForm.prototype.SelectHobbie = function(hobbie)
+	{
+
+		$('[name="hobbies[]"][id="' + this.hobbies[hobbie] + '"]').click();
+		$('[name="hobbies[]"][id="' + this.hobbies[hobbie] + '" ]').click();
+
+	}
+
+
+
+
+
+
+
+
+/*
+// myForm = new SampleForm();
+// myForm.setHobbies('Skiing', 'Running')
+
+
+// myForm.setName($('[name="fullname"]').val("Rebeca Vargas"))
+
+// myForm.setEmail($('[name="email"]').val("rebecavg@gmail.com"))
+
+// myForm.selectGender($('[name="gender"][value="F"]').click())
+
+// myForm.selecthobby1($('[name="hobbies[]"][id="hobby1"]').click())
+// myForm.selecthobby2($('[name="hobbies[]"][id="hobby2"]').click())
+
+
+*/
